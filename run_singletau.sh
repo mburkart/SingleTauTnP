784bef186dd27113510c16ca53d475fcf04e9b4c
Maximilian Burkart <maxiburkart@gmail.com>
2019-11-25 14:45:33 +0100

    Update top level bash script.

--- run_singletau.sh
+++ run_singletau.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 
 ERA=$1
+
+declare -a WPs=("vvvloose" "vvloose" "vloose" "loose" "medium" "tight" "vtight" "vvtight")
 source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
 
-python singletau_effs.py -i /ceph/mburkart/trigger_studies/singletau/ -o output_singletau_$ERA.root -e $ERA -w vloose loose medium tight vtight vvtight
+python singletau_effs.py -i /ceph/mburkart/trigger_studies/singletau_deeptau/ -o output_singletau_$ERA.root -e $ERA -w ${WPs[@]} --bin-by-wp
 
-python plot_singletau.py -i output_singletau_$ERA.root -e $ERA -w vloose loose medium tight vtight vvtight
+for WP in ${WPs[@]}; do
+    python plot_singletau.py -i output_singletau_$ERA.root -e $ERA -w $WP
+done
 
-python utils/extract_singletau_efficiencies.py -i output_singletau_$ERA.root -o SingleTauEfficiencies_$ERA.root -w vloose loose medium tight vtight vvtight
+python utils/extract_singletau_efficiencies.py -i output_singletau_$ERA.root -o SingleTauEfficiencies_$ERA.root -w ${WPs[@]}
 
-for WP in "vloose" "loose" "medium" "tight" "vtight" "vvtight"; do
+for WP in ${WPs[@]}; do
     python plot_proposal_singletau.py -i SingleTauEfficiencies_$ERA.root -e $ERA -w $WP
 done
 
